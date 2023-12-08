ab42bce9edf036083c61a517c70db931b20eb272
Shuntaro Aoki <s_aoki@i.kyoto-u.ac.jp>
2023-12-08 17:26:29 +0900

    [misc] misc fix

--- THINGS-fMRI/download.sh
+++ THINGS-fMRI/download.sh
@@ -3,6 +3,6 @@
 set -Ceu
 
 if [ ! -e "./src/fMRI-Single-Trial-Responses-table-format/betas_csv" ]; then
-    #wget https://plus.figshare.com/ndownloader/files/36789690 -O ./src/fMRI-Single-Trial-Responses-table-format/betas_csv.tar.gz
+    wget https://plus.figshare.com/ndownloader/files/36789690 -O ./src/fMRI-Single-Trial-Responses-table-format/betas_csv.tar.gz
     tar xzvf ./src/fMRI-Single-Trial-Responses-table-format/betas_csv.tar.gz -C ./src/fMRI-Single-Trial-Responses-table-format
 fi
